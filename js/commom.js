f6eded0763142ec7353604c5b7e53c470d0c1e2e
AbbyDing <abbyd0208@gmail.com>
2021-09-02 07:34:48 +0800

    1. 把index.html下方的js移到common.js內 2.調整動畫 3.調整背景圖

---
+++ js/commom.js
@@ -0,0 +1,171 @@
+$(document).ready(function () {
+    var Timer = null;
+    var IsClose = false;
+    var navH 
+    // 監聽網頁，滾動至350 顯示 sticky_nav
+    $(window).scroll(function () {
+        // Alvin 08-26 改用 throttle 參考網址 https://medium.com/@steven234/throttle%E8%B7%9F-debounce%E6%9C%89%E4%BB%80%E9%BA%BC%E5%8D%80%E5%88%A5-e0b1979b1b4f
+
+        if (IsClose) {
+            return
+        }
+        IsClose = true
+        Timer = setTimeout(function () {
+            let scrollTop = $(this).scrollTop();
+            navH = $(".navbar").outerHeight();
+            // 0830 Alvin	
+            if (scrollTop > navH) {
+                $(".navbar-collapse").removeClass("show")
+                $(".navbar-toggler").removeClass("open")
+                $(".sticky_nav").addClass("show")
+                $(".sticky_btn").addClass("show")
+                IsClose = false
+            } else {
+                $(".sticky_nav").removeClass("show")
+                $(".sticky_btn").removeClass("show")
+                $(".sticky_nav").removeClass("open")
+                $(".sticky_btn").removeClass("open")
+                IsClose = false
+            }
+        }, 0);
+    })
+
+    // 漢堡
+    $(".navbar-toggler").on("click", function () {
+        // 漢堡切換 icon 和底色
+        $(".navbar-toggler").toggleClass("open")
+    })
+
+    // 0830 Alvin
+    $(".sticky_btn").on("click", function () {
+        // 漢堡切換 icon 和底色
+        $(".sticky_btn").toggleClass("open")
+        $(".sticky_nav").toggleClass("open")
+    })
+
+    //文章縮放
+    $(".flip").on("click", function () {
+        $(this).parent().toggleClass("unfold")
+    })
+
+    // banner
+    $('.banner_slider').slick({
+        lazyLoad: 'ondemand',
+        arrows: false,
+        dots: true
+    });
+
+    // 公益成果
+    $('.works_slide').slick({
+        lazyLoad: 'ondemand',
+        centerMode: true,
+        centerPadding: '350px',
+        slidesToShow: 1,
+        variableWidth: false,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1550,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '200px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+    // 
+    $('.platform_slider').slick({
+        lazyLoad: 'ondemand',
+        centerMode: true,
+        centerPadding: '25%',
+        slidesToShow: 1,
+        variableWidth: false,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+          autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '15%',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 427,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '10%',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '8%',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+});
+
+// todo scroll magic
+$(function(){
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+    // create a scene
+    var scene = new ScrollMagic.Scene({
+        triggerElement:'.about_conent',
+    
+    })
+    .setClassToggle('.about_conent','active')
+    .addTo(controller); // assign the scene to the controller
+
+    var scene2 = new ScrollMagic.Scene({
+        triggerElement:'.intro_content',
+    })
+    .setClassToggle('.intro_content','active')
+    .addTo(controller); 
+  
+
+    var scene3 = new ScrollMagic.Scene({
+        triggerElement:'.feature_content',
+    })
+    .setClassToggle('.feature_content','active')
+    .addTo(controller); 
+
+    var scene4 = new ScrollMagic.Scene({
+        triggerElement:'.feature_content_2',
+    })
+    .setClassToggle('.feature_content_2','active')
+    .addTo(controller); 
+
+    var scene5 = new ScrollMagic.Scene({
+        triggerElement:'#main_end_animate',
+    })
+    .setClassToggle('#main_end_animate','active')
+    .addTo(controller); 
+
+    controller.addScene([scene, scene2, scene3,scene4,scene5])
+
+});
