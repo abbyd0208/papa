cea47db03c28ec1ec25486ffdf90a207750330e5
Alvin <goape7914@gmail.com>
2021-09-02 20:31:33 +0800

    fix footer animate

--- js/commom.js
+++ js/commom.js
@@ -1,58 +1,58 @@
 $(document).ready(function () {
     var Timer = null;
     var IsClose = false;
-    var navH 
+    var navH;
     // 監聽網頁，滾動至350 顯示 sticky_nav
     $(window).scroll(function () {
         // Alvin 08-26 改用 throttle 參考網址 https://medium.com/@steven234/throttle%E8%B7%9F-debounce%E6%9C%89%E4%BB%80%E9%BA%BC%E5%8D%80%E5%88%A5-e0b1979b1b4f
 
         if (IsClose) {
-            return
+            return;
         }
-        IsClose = true
+        IsClose = true;
         Timer = setTimeout(function () {
             let scrollTop = $(this).scrollTop();
-            navH = $(".navbar").outerHeight();
-            // 0830 Alvin	
+            navH = $('.navbar').outerHeight();
+            // 0830 Alvin
             if (scrollTop > navH) {
-                $(".navbar-collapse").removeClass("show")
-                $(".navbar-toggler").removeClass("open")
-                $(".sticky_nav").addClass("show")
-                $(".sticky_btn").addClass("show")
-                IsClose = false
+                $('.navbar-collapse').removeClass('show');
+                $('.navbar-toggler').removeClass('open');
+                $('.sticky_nav').addClass('show');
+                $('.sticky_btn').addClass('show');
+                IsClose = false;
             } else {
-                $(".sticky_nav").removeClass("show")
-                $(".sticky_btn").removeClass("show")
-                $(".sticky_nav").removeClass("open")
-                $(".sticky_btn").removeClass("open")
-                IsClose = false
+                $('.sticky_nav').removeClass('show');
+                $('.sticky_btn').removeClass('show');
+                $('.sticky_nav').removeClass('open');
+                $('.sticky_btn').removeClass('open');
+                IsClose = false;
             }
         }, 0);
-    })
+    });
 
     // 漢堡
-    $(".navbar-toggler").on("click", function () {
+    $('.navbar-toggler').on('click', function () {
         // 漢堡切換 icon 和底色
-        $(".navbar-toggler").toggleClass("open")
-    })
+        $('.navbar-toggler').toggleClass('open');
+    });
 
     // 0830 Alvin
-    $(".sticky_btn").on("click", function () {
+    $('.sticky_btn').on('click', function () {
         // 漢堡切換 icon 和底色
-        $(".sticky_btn").toggleClass("open")
-        $(".sticky_nav").toggleClass("open")
-    })
+        $('.sticky_btn').toggleClass('open');
+        $('.sticky_nav').toggleClass('open');
+    });
 
     //文章縮放
-    $(".flip").on("click", function () {
-        $(this).parent().toggleClass("unfold")
-    })
+    $('.flip').on('click', function () {
+        $(this).parent().toggleClass('unfold');
+    });
 
     // banner
     $('.banner_slider').slick({
         lazyLoad: 'ondemand',
         arrows: false,
-        dots: true
+        dots: true,
     });
 
     // 公益成果
@@ -71,8 +71,8 @@ $(document).ready(function () {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '200px',
-                    slidesToShow: 1
-                }
+                    slidesToShow: 1,
+                },
             },
             {
                 breakpoint: 992,
@@ -80,12 +80,13 @@ $(document).ready(function () {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '0px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    })
-    // 
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+
+    // 平台三大特色
     $('.platform_slider').slick({
         lazyLoad: 'ondemand',
         centerMode: true,
@@ -94,8 +95,8 @@ $(document).ready(function () {
         variableWidth: false,
         arrows: true,
         dots: true,
-        autoplay: true,
-          autoplaySpeed: 3000,
+        // autoplay: true,
+        //autoplaySpeed: 3000,
         responsive: [
             {
                 breakpoint: 576,
@@ -103,8 +104,8 @@ $(document).ready(function () {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '15%',
-                    slidesToShow: 1
-                }
+                    slidesToShow: 1,
+                },
             },
             {
                 breakpoint: 427,
@@ -112,8 +113,8 @@ $(document).ready(function () {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '10%',
-                    slidesToShow: 1
-                }
+                    slidesToShow: 1,
+                },
             },
             {
                 breakpoint: 360,
@@ -121,51 +122,85 @@ $(document).ready(function () {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '8%',
-                    slidesToShow: 1
-                }
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+});
+
+function debounce(func, delay = 250) {
+    let timer = null;
+
+    return () => {
+        let context = this;
+        let args = arguments;
+
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+    };
+}
+
+// navbar點擊滾動效果
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname &&
+            this.hash.slice(1) != 'top'
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000
+                );
+                return false;
             }
-        ]
-    })
+        }
+    });
 });
 
 // todo scroll magic
-$(function(){
+$(function () {
     // init controller
     var controller = new ScrollMagic.Controller();
 
     // create a scene
     var scene = new ScrollMagic.Scene({
-        triggerElement:'.about_conent',
-    
+        triggerElement: '.about_conent',
     })
-    .setClassToggle('.about_conent','active')
-    .addTo(controller); // assign the scene to the controller
+        .setClassToggle('.about_conent', 'active')
+        .addTo(controller); // assign the scene to the controller
 
     var scene2 = new ScrollMagic.Scene({
-        triggerElement:'.intro_content',
+        triggerElement: '.intro_content',
     })
-    .setClassToggle('.intro_content','active')
-    .addTo(controller); 
-  
+        .setClassToggle('.intro_content', 'active')
+        .addTo(controller);
 
     var scene3 = new ScrollMagic.Scene({
-        triggerElement:'.feature_content',
+        triggerElement: '.feature_content',
     })
-    .setClassToggle('.feature_content','active')
-    .addTo(controller); 
+        .setClassToggle('.feature_content', 'active')
+        .addTo(controller);
 
     var scene4 = new ScrollMagic.Scene({
-        triggerElement:'.feature_content_2',
+        triggerElement: '.feature_content_2',
     })
-    .setClassToggle('.feature_content_2','active')
-    .addTo(controller); 
+        .setClassToggle('.feature_content_2', 'active')
+        .addTo(controller);
 
     var scene5 = new ScrollMagic.Scene({
-        triggerElement:'#main_end_animate',
+        triggerElement: '#main_end_animate',
     })
-    .setClassToggle('#main_end_animate','active')
-    .addTo(controller); 
+        .setClassToggle('#main_end_animate', 'active')
+        .addTo(controller);
 
-    controller.addScene([scene, scene2, scene3,scene4,scene5])
-
+    controller.addScene([scene, scene2, scene3, scene4, scene5]);
 });
