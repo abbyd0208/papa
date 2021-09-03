a7344f832f4c0978eba18430e4075dc7b11e0c2f
AbbyDing <abbyd0208@gmail.com>
2021-09-03 08:04:59 +0800

    loading動畫完成

--- js/commom.js
+++ js/commom.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+
+    setTimeout(function(){
+        $('.loading').css({'visibility':'hidden', 'opacity':0});
+        $('.wrapper').removeClass('cover')
+    },3000);
+
+
+
+
     var Timer = null;
     var IsClose = false;
     var navH;
